6aaf0581757b898e6e8db266f33871bcb8936f17
Matthew Roesener <roesenerm@gmail.com>
2017-03-17 15:45:46 -0700

    testing - removed payment method

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from two1.bitserv.django import payment
 
-@api_view(['GET'])
-@payment.required(100)
+#@api_view(['GET'])
+#@payment.required(100)
 def buy(request):
 	return HttpResponse('Hello 21!', status=200)
